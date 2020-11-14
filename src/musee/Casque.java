62e9173c929c87b0ba6b6c8bc20b539d9602f412
AlexandreMorel <64785022+AlexandreMorel@users.noreply.github.com>
2020-11-14 21:58:21 +0100

    Update Casque.java

--- src/musee/Casque.java
+++ src/musee/Casque.java
@@ -3,6 +3,8 @@ package musee;
 import combat.Combat;
 import village.Habitant;
 
+import java.util.UUID;
+
 public class Casque {
 	String grade;
 	String forme;
@@ -10,6 +12,7 @@ public class Casque {
 	Combat combat;
 	boolean presentAuMusee;
 	Habitant habitant;
+	UUID uuid;
 
 	public Casque(String grade, String forme, String etat, Combat combat, boolean presentAuMusee) {
 		super();
@@ -18,6 +21,15 @@ public class Casque {
 		this.etat = etat;
 		this.combat = combat;
 		this.presentAuMusee = presentAuMusee;
+		this.uuid = UUID.randomUUID();
+	}
+	
+	public void setPresentAuMusee(boolean presentAuMusee) {
+		this.presentAuMusee = presentAuMusee;
+	}
+	
+	public UUID getUuid() {
+		return uuid;
 	}
 
 }
